9e84db999c753fc2544fdd1065b2f217be42263c
Rofix <frankiewb9527@gmail.com>
2024-07-20 23:10:07 +0800

    create introduce

--- src/views/introduce.vue
+++ src/views/introduce.vue
@@ -3,5 +3,12 @@
 <template>
   <div>
     <h1>Vue.js 定义</h1>
+    <ul>
+      <li>声明式渲染</li>
+      <li>响应式系统</li>
+      <li>模块组件化</li>
+      <li>渐进式使用</li>
+      <li>单文件组件</li>
+    </ul>
   </div>
 </template>
